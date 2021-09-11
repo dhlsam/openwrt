f356575e5bcd183ebfd5513a05bb673ad5c09ab9
dhlsam <dhlsam1987@gmail.com>
2021-09-11 16:37:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+
+# 设置密码为空
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
