d53c124b702be9fe43cb89a084048f69bfb2fe7f
dhlsam <dhlsam1987@gmail.com>
2021-09-12 17:19:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,6 +25,6 @@ sed -i "5i uci set network.lan.type='bridge'" ./package/lean/default-settings/fi
 sed -i "6i uci set network.lan.ifname='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
 sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
 sed -i "8i uci set network.lan.gateway='10.10.10.10.11'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
-sed -i "9i uci set network.lan.dns='127.0.0.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+sed -i "9i uci set network.lan.dns='114.114.114.114 223.5.5.5'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
 sed -i "10i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
 
